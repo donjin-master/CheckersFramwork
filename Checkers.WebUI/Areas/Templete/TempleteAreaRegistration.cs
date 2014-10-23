66e7a58073c1fa57f382e6d8aa9778f5decfba9e
decadestory <728246447@qq.com>
2014-10-23 16:15:51 +0800

    初始化

---
+++ Checkers.WebUI/Areas/Templete/TempleteAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Checkers.WebUI.Areas.Templete
+{
+    public class TempleteAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Templete";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Templete_default",
+                "Templete/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
